e530920d1b6ec05a89c758842f817fa888bc8b9e
Asish Sarangi <asishsarangi2005@gmail.com>
2025-06-24 15:39:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
     <>
       <div>
         <h1>This code is from the devops class</h1>
+        <h2>CI/CD Pipeline concept</h2>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
